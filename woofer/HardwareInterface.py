7a519d386d06ff3cdfe1e267f9e64d75741bc432
Aaron Schultz <aaron@hschultz.net>
2020-02-09 20:11:54 -0800

    Introduced axes to hardware interface

--- woofer/HardwareInterface.py
+++ woofer/HardwareInterface.py
@@ -12,11 +12,15 @@ class HardwareInterface:
         for sn in ODRIVE_SERIAL_NUMBERS:
             o = odrive.find_any(serial_number=sn)
             self.odrives.append(o)
+        assert len(self.odrives) == 6
         calibrate_odrives(self.odrives)
         set_position_control(self.odrives)
 
+        self.axes = [[None for _ in range(3)] for _ in range(4)]
+        assign_axes(self.odrives, self.axes)
+
     def set_actuator_postions(self, joint_angles):
-        set_all_odrive_positions(self.odrives, joint_angles)
+        set_all_odrive_positions(self.axes, joint_angles)
 
 
 def calibrate_odrives(odrives):
@@ -34,8 +38,14 @@ def set_position_control(odrives):
         odrv.axis1.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
 
 
-def set_all_odrive_positions(odrives, joint_angles):
-    pass
+def assign_axes(odrives, axes):
+    assert False  # YOU MUST MAKE ABSOLUTELY SURE THIS MATCHES THE WIRING
+
+
+def set_all_odrive_positions(axes, joint_angles):
+    for i in range(len(joint_angles)):
+        for j in range(len(joint_angles[i])):
+            axes[i][j].controller.pos_setpoint = joint_angles[i][j]
 
 
 def radians_to_encoder_count(angle):
