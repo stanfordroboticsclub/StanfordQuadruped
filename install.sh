08f1d10d1a02ab992088337d4cab0b5531b66703
Nathan Kau <nathankau@gmail.com>
2020-03-07 17:38:30 -0800

    update install script

---
+++ install.sh
@@ -0,0 +1,19 @@
+yes | sudo apt-get install libatlas-base-dev
+yes | pip3 install numpy scipy transforms3d pigpio pyserial
+yes | pip install numpy scipy transforms3d pigpio pyserial
+yes | sudo pip install numpy scipy transforms3d pigpio pyserial
+cd ..
+git clone https://github.com/stanfordroboticsclub/PupperCommand.git
+cd PupperCommand
+sudo bash install.sh
+cd ..
+git clone https://github.com/stanfordroboticsclub/PS4Joystick.git
+cd PS4Joystick
+sudo bash install.sh
+cd ..
+sudo systemctl enable joystick
+wget https://github.com/joan2937/pigpio/archive/v74.zip
+unzip v74.zip
+cd pigpio-74
+make
+sudo make install
