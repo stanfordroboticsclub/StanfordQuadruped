3e4e3ae26102bb69c0c3850fb27ac049fb574cfb
Florian Golemo <fgolemo@gmail.com>
2020-08-28 19:06:04 -0400

    cleanup - we don't need the woofer for what we're doing here and in case we have it in the git hostory

---
+++ scripts/09-test-random-steps.py
@@ -0,0 +1,6 @@
+STAIRS_STEP_WIDTH = 1  # if you're standing in front of a staircase and looking up, what's the left-right distance?
+STAIRS_STEP_DEPTH = 0.1  # if you're stepping onto one step, how much of your foot can fit on the step?
+STAIRS_STEP_HEIGHT = 0.2  # how far for you have to go up between each step?
+
+sim =
+
