5bb041a50d6f2908be53d8125c194146a2c944de
Nathan Kau <nathankau@gmail.com>
2020-09-30 22:07:50 -0700

    add dependencies to library

--- lib/stanford_quadruped_controller/setup.py
+++ lib/stanford_quadruped_controller/setup.py
@@ -6,11 +6,15 @@ setuptools.setup(
     author="Nathan Kau",
     author_email="nathankau@gmail.com",
     description="Core functions for controlling quadruped robots",
-    packages=['stanford_quadruped_controller'],
+    packages=["stanford_quadruped_controller"],
+    install_requires=[
+        "numpy",
+        "pyyaml",
+        "transforms3d",
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
 )
-
