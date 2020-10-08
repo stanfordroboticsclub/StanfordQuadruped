2a293ba97a16f3673f2d591437039f73b58dcd17
Nathan Kau <nathankau@gmail.com>
2020-10-07 20:11:30 -0700

    delete stuff in the init file to stop importing udpcomms

--- lib/stanford_quadruped_controller/stanford_quadruped_controller/__init__.py
+++
@@ -1,8 +0,0 @@
-import stanford_quadruped_controller.config
-import stanford_quadruped_controller.controller
-import stanford_quadruped_controller.gait_controller
-import stanford_quadruped_controller.hardware_interface
-import stanford_quadruped_controller.stance_controller
-import stanford_quadruped_controller.state
-import stanford_quadruped_controller.swing_leg_controller
-import stanford_quadruped_controller.joystick_interface
