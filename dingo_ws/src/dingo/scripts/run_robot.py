2ac9fab84ecc7852c6809e031ba2eab8695cfd86
Yerbert <acal0003@student.monash.edu>
2023-04-13 18:43:49 +1000

    Added simulation packages into the repo. Begun integration of them into pupper code, including references to dingo and arguments for run_robot which tell it whether to run the sim and/or the hardware interface

--- dingo_ws/src/dingo/scripts/run_robot.py
+++ dingo_ws/src/dingo/scripts/run_robot.py
@@ -1,11 +1,12 @@
 import numpy as np
 import time
 import rospy
+import sys
 from dingo_nano_interfacing.IMU import IMU
 from dingo_control.Controller import Controller
 from dingo_joystick_interfacing.JoystickInterface import JoystickInterface
 from dingo_control.State import State
-#from dingo_servo_interfacing.HardwareInterface import HardwareInterface
+from dingo_servo_interfacing.HardwareInterface import HardwareInterface
 from dingo_servo_interfacing.Config import Configuration
 from dingo_control.Kinematics import four_legs_inverse_kinematics
 
@@ -16,14 +17,43 @@ def main(use_imu=False):
     message_rate = 50
     rate = rospy.Rate(message_rate)
 
+    #Fetching is_sim and is_physical from arguments
+    args = rospy.myargv(argv=sys.argv)
+    if len(args) != 2: #arguments have not been provided, go to defaults (not sim, is physical)
+        is_sim = 0
+        is_physical = 1
+    else:
+        is_sim = args[0]
+        is_physical = args[1]
+
+    #TODO: Create a publisher for joint states 
+    if is_sim:
+        command_topics = ["/notspot_controller/FR1_joint/command",
+                  "/notspot_controller/FR2_joint/command",
+                  "/notspot_controller/FR3_joint/command",
+                  "/notspot_controller/FL1_joint/command",
+                  "/notspot_controller/FL2_joint/command",
+                  "/notspot_controller/FL3_joint/command",
+                  "/notspot_controller/RR1_joint/command",
+                  "/notspot_controller/RR2_joint/command",
+                  "/notspot_controller/RR3_joint/command",
+                  "/notspot_controller/RL1_joint/command",
+                  "/notspot_controller/RL2_joint/command",
+                  "/notspot_controller/RL3_joint/command"]
+
+        publishers = []
+        for i in range(len(command_topics)):
+            publishers.append(rospy.Publisher(command_topics[i], Float64, queue_size = 0))
+
     # Create config
     config = Configuration()
-    #hardware_interface = HardwareInterface()
 
-    # Create imu handle
-    if use_imu:
-        imu = IMU(port="/dev/ttyACM0")
-        imu.flush_buffer()
+    if is_physical:
+        hardware_interface = HardwareInterface()
+        # Create imu handle
+        if use_imu:
+            imu = IMU(port="/dev/ttyACM0")
+            imu.flush_buffer()
 
     # Create controller and user input handles
     controller = Controller(
@@ -76,12 +106,19 @@ def main(use_imu=False):
             # Step the controller forward by dt
             controller.run(state, command)
 
+            #TODO here: publish the joint values (in state.joint_angles) to a publisher
+            #If running simulator, publish joint angles to gazebo controller:
+            if is_sim:
+                for i in range(len(joint_angles)):
+                    publishers[i].publish(joint_angles[i])
+            
+            if is_physical:
+                # Update the pwm widths going to the servos
+                hardware_interface.set_actuator_postions(state.joint_angles)
+            
             time.end = rospy.Time.now()
             #Uncomment following line if want to see how long it takes to execute a control iteration
             #print(str(time.start-time.end))
             rate.sleep()
-            # Update the pwm widths going to the servos
-            #hardware_interface.set_actuator_postions(state.joint_angles)
-
 
 main()
